95b3301ecf0e9b27aa8fd172ba57c609fa2ac941
yulongbb <sunyulongbb@gmail.com>
2019-11-27 14:34:25 +0800

    项目初始化

---
+++ src/main/java/com/yulongbb/codeproject/CodeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.yulongbb.codeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yulongbb/codeproject/CodeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yulongbb.codeproject;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
